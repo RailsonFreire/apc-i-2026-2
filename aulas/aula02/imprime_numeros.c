adcc4c5aa95ecb4b6acbc28e4ca948d9c5607bb2
Railson Robert <railsonfreire444@gmail.com>
2026-08-26 21:53:13 -0300

    codigo da aula2

--- aulas/aula02/imprime_numeros.c
+++ aulas/aula02/imprime_numeros.c
@@ -7,8 +7,20 @@ int main  () {
     printf("8.5");
     printf("10 + 5");
     printf("%i\n", 10 + 5);
-    printf("%f", 8.5);
-    printf("%1f", 8.5);
+    printf("%f\n", 8.5);
+    printf("%.1f\n\n", 8.5);
+
+
+    printf("CPF = %i\n", 11111111111);
+    printf("CPF = %lli\n", 11111111111);
+    // preenche com espaço ate 11 digitos
+    printf("CPF = %11i\n", 00000000001);
+    // preenche com 0 ate 11 digitos
+    printf("CPF = %011i\n", 00000000001);
+
+    printf("Preco = R$ %7.2f\n", 100.00);
+    printf("Preco = R$ %7.2f\n", 10.0);
+    printf("Preco = R$ %7.2f\n", 1000.0);
 
     return 0;
 }
--- aulas/aula02/imprime_texto.c
+++ aulas/aula02/imprime_texto.c
@@ -3,7 +3,34 @@
 int main () {
 
     // funcao para escrever algo na tela    
-    printf("C e muito legal!\n\n\n\n\n\n\n\n\n"); // pular linha 
-    printf("Vamos codar");
-return 0;
+    printf("C e muito legal!\n\n"); // pular linha 
+    
+    printf("Vamos codar\n");
+    
+    // ""eh texto (string)  '' eh caracter
+    printf("A terceira letra do alfabeto eh %c e a ultima eh %c\n", 'c', 'z'); 
+    printf("Ola, %s!\n", "Railson");
+
+
+
+    printf("=================================================\n");
+    printf("                  NOTA LEGAL                       ");
+    printf("=================================================\n");
+    printf("Caneta                   Unid      Preco       Valor\n");
+    printf("Caneta                  1         1,00       1,00\n");
+    printf("Caneta                  1         5,00       1,00\n");
+    printf("Caneta                  2         2,50       1,00\n");
+    printf("-------------------------------------------------\n");
+    printf("TOTAL: R$ 11,00\n");
+    printf("---------------------------------------------------");
+    
+    
+    
+    
+
+
+
+
+
+    return 0;
 }
